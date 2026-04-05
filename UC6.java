0016509146c5c165520f994d53e9d91db2b002ee
Yashvi Agrahari <yashviagrahari0@gmail.com>
2026-04-05 22:02:59 +0530

    Refactor HelloApp to improve greeting logic
    Refactor HelloApp to handle no arguments case and improve greeting format.

--- UC6.java
+++ UC6.java
@@ -1,19 +1,15 @@
-public class HelloApp{
-	public static void main(String[] args){
-		StringBuilder name = new StringBuilder();
-		boolean first =true;
-		for (String name : args){
-			if (!first){
-				name.append(", ");
-			}
-			name.append(name);
-			first =false;
-		}
-		String name = names.toString();
-        if (args.length !=0) {
-            System.out.println("Hello, " + name + "!");
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello, " + "World" + "!");
+            String greeting = "Hello, ";
+            for (String name : args) {
+                greeting += name + ", ";
+            }
+            greeting = greeting.substring(0, greeting.length() - 2) + "!";
+            
+            System.out.println(greeting);
         }
-	}
+    }
 }
