f6fc5cbddef81ba081bbf9c825587d9087496ca6
Yashvi Agrahari <yashviagrahari0@gmail.com>
2026-04-05 21:33:06 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,17 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
